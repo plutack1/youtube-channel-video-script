eee8c010e062adfcaceb3f3c3ebcd124172d5ffa
Talut Salako <71020299+plutack1@users.noreply.github.com>
2023-07-31 07:20:43 +0000

    multiple pages

--- main.py
+++ main.py
@@ -7,19 +7,30 @@ with open("secrets.txt") as f:
 
 channel_id = channel_id
 api_key = api_key
+next_page_token = ""
 url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + channel_id + "&key=" + api_key + "&part=contentDetails"
 response = requests.get(url)
 data = json.loads(response.text)
 upload_id = data["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
 
-url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=" + upload_id + "&key=" + api_key
-response = requests.get(url)
-data = json.loads(response.text)
+
 with open("videos.csv", "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
     writer.writerow(["Title", "Publish Date"])
 
-    for item in data["items"]:
-        title = item["snippet"]["title"]
-        publish_date = item["snippet"]["publishedAt"]
-        writer.writerow([title, publish_date])
+    while True:
+        url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=" + upload_id + "&key=" + api_key + "&pageToken=" + next_page_token
+        response = requests.get(url)
+        data = json.loads(response.text)
+
+        for item in data["items"]:
+            title = item["snippet"]["title"]
+            description = item["snippet"]["description"]
+            published_at = item["snippet"]["publishedAt"]
+            video_url = "https://www.youtube.com/watch?v=" + item["snippet"]["resourceId"]["videoId"]
+            writer.writerow([title, description, published_at, video_url])
+
+        if "nextPageToken" in data:
+            next_page_token = data["nextPageToken"]
+        else:
+            break
