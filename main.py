28e7f6306eb58b8cf847088a0952c0c1b99b18d0
Talut Salako <71020299+plutack1@users.noreply.github.com>
2023-08-01 05:55:58 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+import os
+from dotenv import load_dotenv
 import requests
+
 import json
 import csv
-with open("secrets.txt") as f:
-    config = compile(f.read(), "config.txt", "exec")
-    exec(config)
 
-channel_id = channel_id
-api_key = api_key
+channel_id = os.getenv("channel_id")
+api_key = os.getenv("api_key")
+
+print(channel_id)
+print(api_key)
 next_page_token = ""
 url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + channel_id + "&key=" + api_key + "&part=contentDetails"
 response = requests.get(url)
-data = json.loads(response.text)
-upload_id = data["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+data = response.json()
+upload_id = data["uploads"]
 
 
 with open("videos.csv", "w", newline="", encoding="utf-8") as f:
@@ -21,14 +24,14 @@ with open("videos.csv", "w", newline="", encoding="utf-8") as f:
     while True:
         url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=" + upload_id + "&key=" + api_key + "&pageToken=" + next_page_token
         response = requests.get(url)
-        data = json.loads(response.text)
+        data = response.json()
 
-        for item in data["items"]:
-            title = item["snippet"]["title"]
-            published_at = item["snippet"]["publishedAt"]
+        for item in data:
+            title = item["title"]
+            published_at = item["publishedAt"]
             writer.writerow([title, published_at])
 
-        if "nextPageToken" in data:
-            next_page_token = data["nextPageToken"]
+        # if "nextPageToken" in data:
+        #     next_page_token = data["nextPageToken"]
         else:
             break
